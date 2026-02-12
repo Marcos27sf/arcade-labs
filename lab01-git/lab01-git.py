83a4240a38d993c17411e5b8b6297d0cf0cbea49
Marcos27sf <marcos.serranof@edu.uah.es>
2026-02-12 16:30:56 +0100

    Cambio desde local

--- lab01-git/lab01-git.py
+++ lab01-git/lab01-git.py
@@ -3,12 +3,10 @@ import arcade
 WIDTH = 800
 HEIGHT = 600
 
-arcade.open_window(WIDTH, HEIGHT, "Example")
+arcade.open_window(WIDTH, HEIGHT, "Ejemplo")
 
 arcade.start_render()
 arcade.draw_text("Hello, world", 350, 300, arcade.color.WHITE)
 arcade.finish_render()
-
-print("Modificación")
 
 arcade.run()
