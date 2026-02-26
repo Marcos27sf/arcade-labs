0beb263705148360625621d3efd97ece092f4ccf
Marcos27sf <marcos.serranof@edu.uah.es>
2026-02-26 16:31:18 +0100

    Mejoras de modularidad

--- lab03-preguntas/lab03-json.py
+++ lab03-preguntas/lab03-json.py
@@ -1,45 +1,10 @@
 from pathlib import Path
-from random import shuffle
 import json
+from main import jugar
 
 path = Path("preguntas.json")
 
 preguntas = []
-
-def extraer_preguntas(pregunta: str) -> dict:
-    pregunta_segmentada = pregunta.split("|")
-    pregunta_dict = {}
-
-    pregunta_dict["pregunta"] = pregunta_segmentada[0]
-    pregunta_dict["correcta"] = pregunta_segmentada[1]
-    pregunta_dict["opciones"] = pregunta_segmentada[1:]
-    
-    return pregunta_dict
-
-def jugar(preguntas: list) -> None:
-    puntos = 0
-    letras = ["a", "b", "c", "d"]
-
-    for pregunta in preguntas:
-        shuffle(pregunta["opciones"])
-        print(pregunta["pregunta"])
-
-        for letra, opcion in zip(letras, pregunta["opciones"]):
-            print(f"{letra}. {opcion}")
-        
-        eleccion_valida = False
-        while not eleccion_valida:
-            try:
-                eleccion = input("Introduce tu respuesta: ").lower()
-                index_eleccion = letras.index(eleccion)
-                if pregunta["opciones"][index_eleccion] == pregunta["correcta"]:
-                    puntos += 5
-                eleccion_valida = True
-            except:
-                print("Opción no válida")
-    
-    print(f"La puntuación obtenida ha sido: {puntos}")
-
 
 with open(path, "r", encoding="utf-8") as file:
     preguntas = json.load(file)
